e0191c777498b4edcda8b4a18f8b1c84da2055a9
bwestwood11 <dabrettwestwood@gmail.com>
2023-10-24 01:30:20 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,20 @@
 import CTA from '@/components/Call-To-Action'
 import Logos from '@/components/Cloud-Logos'
-import Features from '@/components/Features'
+
 import Hero from '@/components/Hero'
-import Hero2 from '@/components/Hero2'
 import MortgagePrograms from '@/components/Mortgage-Programs'
 import Reviews from '@/components/Review'
-import Review from '@/components/Review'
 import WhyUs from '@/components/Why-Us'
 
 
 export default function Home() {
   return (
     <section>
-      <Hero />
-      {/* <Hero2 /> */}
+      <Hero /> 
       <Logos />
       <MortgagePrograms />
       <Reviews />
       <WhyUs />
-      {/* <Features /> */}
      <CTA />
     </section>
   )
