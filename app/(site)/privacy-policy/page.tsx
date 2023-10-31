3276f5bd937209fcaf05cef962953182b07d18d9
bwestwood11 <dabrettwestwood@gmail.com>
2023-10-31 13:22:48 -0400

    changes

--- app/(site)/privacy-policy/page.tsx
+++ app/(site)/privacy-policy/page.tsx
@@ -7,13 +7,13 @@ const PrivacyPage = () => {
                <h1 className='text-4xl font-extrabold mt-8'>Privacy Policy</h1>
                <p className='text-lsm font-semibold my-3'>Last updated: 10-23-2023</p>
                <h2 className='text-2xl font-extrabold tracking-wide my-3'>Introduction</h2>
-               <p>
+               <p className='text-gray-600 leading-7'>
                At Waterstreet Mortgage, LLC, we are dedicated to safeguarding your privacy and ensuring the security of your personal information. This Privacy Policy explains how we collect, use, disclose, and protect your information when you use our mortgage services. We do not sell or share your personal information with third parties, except as described in this policy.
                </p>
                <h2 className='text-2xl font-extrabold tracking-wide my-3'>Information We Collect</h2>
                <h3>Personal Information</h3>
-               <p>We may collect personal information from you when you use our services. This information may include, but is not limited to:</p>
-               <ul>
+               <p className='text-gray-600 leading-7'>We may collect personal information from you when you use our services. This information may include, but is not limited to:</p>
+               <ul className='text-gray-600 leading-7'>
                 <li>
                     Name
                 </li>
@@ -28,8 +28,8 @@ const PrivacyPage = () => {
                 </li>
                 </ul>
                 <h2 className='text-2xl font-extrabold tracking-wide my-3'>How We Use Your Information</h2>
-                <p>We use your information for the following purposes:</p>
-                <ul>
+                <p className='text-gray-600 leading-7'>We use your information for the following purposes:</p>
+                <ul className='text-gray-600 leading-7'>
                     <li>
                     To provide you with mortgage services and assistance in securing financing.
                     </li>
@@ -47,8 +47,8 @@ const PrivacyPage = () => {
                     </li>
                 </ul>
                 <h2 className='text-2xl font-extrabold tracking-wide my-3'>Information Sharing</h2>
-                <p>We do not sell, trade, or share your personal information with third parties for marketing purposes. However, we may share your information under the following circumstances:</p>
-                <ul>
+                <p className='text-gray-600 leading-7'>We do not sell, trade, or share your personal information with third parties for marketing purposes. However, we may share your information under the following circumstances:</p>
+                <ul className='text-gray-600 leading-7'>
                     <li>
                     With your consent. 
                     </li>
@@ -60,10 +60,10 @@ const PrivacyPage = () => {
                     </li>
                 </ul>
                 <h2 className='text-2xl font-extrabold tracking-wide my-3'>Information Security</h2>
-                <p>We take security seriously and have implemented measures to protect your information from unauthorized access, disclosure, or alteration. Our employees are trained to safeguard your data, and we use encryption, access controls, and other security protocols.</p>
+                <p className='text-gray-600 leading-7'>We take security seriously and have implemented measures to protect your information from unauthorized access, disclosure, or alteration. Our employees are trained to safeguard your data, and we use encryption, access controls, and other security protocols.</p>
                 <h2 className='text-2xl font-extrabold tracking-wide my-3'>Your Choices</h2>
-                <p>You have the right to:</p>
-                <ul>
+                <p className='text-gray-600 leading-7'>You have the right to:</p>
+                <ul className='text-gray-600 leading-7'>
                     <li>
                     Access and update your personal information.
                     </li>
@@ -75,7 +75,7 @@ const PrivacyPage = () => {
                     </li>
                 </ul>
                 <h2 className='text-2xl font-extrabold tracking-wide my-3'>Contact Us</h2>
-                <p>If you have any questions, concerns, or requests regarding your personal information or this Privacy Policy, please contact us at:</p>
+                <p className='text-gray-600 leading-7 mb-3'>If you have any questions, concerns, or requests regarding your personal information or this Privacy Policy, please contact us at:</p>
                 <p>Waterstreet Mortgage, LLC</p>
                 <p>13506 N. Rome Ave Tampa, FL 33613</p>
                 <p>813-400-5479</p>
--- components/Team.tsx
+++ components/Team.tsx
@@ -1,91 +1,120 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const people = [
-    {
-      name: 'Kevin Tabares',
-      role: 'Broker Owner',
-      imageUrl:
-        '/Kevin.jpg',
-      NMLS: 'NMLS# 1994473',
-      email: 'ktabares@waterstreetmtg.com',
-    },
-    {
-      name: 'Jeffrey Tabares',
-      role: 'Broker Owner',
-      imageUrl:
-        '/Jeffrey.jpg',
-      NMLS: 'NMLS# 2105748',
-      email: 'jtabares@waterstreetmtg.com',  
-    },
-    {
-      name: 'Haley Tubbs',
-      role: 'Mortgage Broker',
-      imageUrl:
-        '/Haley.jpg',
-      NMLS: 'NMLS# 2082764',
-      email: 'ahtubbs@waterstreetmtg.com',
-    },
-   
-  ]
+  {
+    name: "Kevin Tabares",
+    role: "Broker Owner",
+    imageUrl: "/Kevin.jpg",
+    NMLS: "NMLS# 1994473",
+    email: "ktabares@waterstreetmtg.com",
+    linkedIn: "https://www.linkedin.com/in/kevin-tabares/",
+    Instagram:
+      "https://www.instagram.com/therealkevintabares/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA==",
+  },
+  {
+    name: "Jeffrey Tabares",
+    role: "Broker Owner",
+    imageUrl: "/Jeffrey.jpg",
+    NMLS: "NMLS# 2105748",
+    email: "jtabares@waterstreetmtg.com",
+    linkedIn: "https://www.linkedin.com/in/jeffreytabares/",
+    Instagram: "https://www.instagram.com/tabares28k/?hl=en",
+  },
+  {
+    name: "Haley Tubbs",
+    role: "Mortgage Broker",
+    imageUrl: "/Haley.jpg",
+    NMLS: "NMLS# 2082764",
+    email: "ahtubbs@waterstreetmtg.com",
+    linkedIn: "https://www.linkedin.com/in/amandahaleytubbs/",
+    Instagram: "https://www.instagram.com/haytubbs/?hl=en",
+  },
+];
 
 const Team = () => {
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8 pb-16">
-    <div className="mx-auto max-w-2xl lg:mx-0">
-      <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our team</h2>
-      <p className="mt-6 text-lg leading-8 text-gray-600">
-      Meet our dedicated Mortgage team, a dynamic group of professionals committed to delivering exceptional results for our clients.
-      </p>
-    </div>
-    <ul
-      role="list"
-      className="mx-auto mt-20 grid max-w-2xl z-[100] grid-cols-1 gap-x-16 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
-    >
-      {people.map((person) => (
-        <li key={person.name}>
-          <Image className="w-full h-3/4 rounded-2xl object-cover" src={person.imageUrl} alt="" width={500} height={500} priority/>
-          <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
-          <p className="text-base leading-7 text-gray-600">{person.role}</p>
-          <ul role="list" className="mt-6 flex flex-col gap-x-6">
-            <li>
-              <p className='text-gray-600 tracking-wide'>{person.NMLS}</p>  
-            </li>
-            <li>
+      <div className="mx-auto max-w-2xl lg:mx-0">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Our team
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Meet our dedicated Mortgage team, a dynamic group of professionals
+          committed to delivering exceptional results for our clients.
+        </p>
+      </div>
+      <ul
+        role="list"
+        className="mx-auto mt-20 grid max-w-2xl z-[100] grid-cols-1 gap-x-16 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+      >
+        {people.map((person) => (
+          <li key={person.name}>
+            <Image
+              className="w-full h-3/4 rounded-2xl object-cover"
+              src={person.imageUrl}
+              alt=""
+              width={500}
+              height={500}
+              priority
+            />
+            <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">
+              {person.name}
+            </h3>
+            <p className="text-base leading-7 text-gray-600">{person.role}</p>
+            <ul role="list" className="mt-6 flex flex-col gap-x-6">
+              <li>
+                <p className="text-gray-600 tracking-wide">{person.NMLS}</p>
+              </li>
+              <li>
                 <a href={`mailto:${person.email}`}>
-                   <p className='text-gray-600 tracking-wide cursor-pointer'>{person.email}</p>   
+                  <p className="text-gray-600 tracking-wide cursor-pointer">
+                    {person.email}
+                  </p>
                 </a>
-              
-            </li>
-            <div className='flex gap-5 mt-4'>
+              </li>
+              <div className="flex gap-5 mt-4 items-center">
                 <li>
-              <a href={person.NMLS} className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">Twitter</span>
-                <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <a href={person.email} className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">LinkedIn</span>
-                <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                  <path
-                    fillRule="evenodd"
-                    d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
-            </li>
-            </div>
-            
-          </ul>
-        </li>
-      ))}
-    </ul>
-  </div>
-  )
-}
+                  <a
+                    href={person.Instagram}
+                    className="text-gray-400 hover:text-gray-500"
+                  >
+                    <span className="sr-only">Instagram</span>
+                    <Image
+                      src="/instagram.png"
+                      alt="Instagram"
+                      width={25}
+                      height={25}
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={person.linkedIn}
+                    className="text-gray-400 hover:text-gray-500"
+                  >
+                    <span className="sr-only">LinkedIn</span>
+                    <svg
+                      className="h-5 w-5"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </div>
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Team
+export default Team;
