58d9dcbea693d6421bbde56d2ea85cb1ce33bd04
bwestwood11 <dabrettwestwood@gmail.com>
2023-10-31 13:23:03 -0400

    chnages again

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <section>
       <Hero /> 
-      <Logos />
+      {/* <Logos /> */}
       <ShiningStar  />
       <MortgagePrograms />
       <Reviews />
